4eaed81f798634b80cffbed3f83532f00ae84908
Tomas Molina <jtml.mass@gmail.com>
2021-02-08 19:11:11 -0400

    Add User entity and user repository

---
+++ src/main/java/com/edenstower/api/entities/User.java
@@ -0,0 +1,31 @@
+package com.edenstower.api.entities;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Data
+@Entity
+@Table(name = "users")
+@EntityListeners(AuditingEntityListener.class)
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private String username;
+
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @Column(name = "email_address", nullable = false)
+    private String email;
+
+    @Column(name = "created_at", nullable = false)
+    private Date createdAt;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+}

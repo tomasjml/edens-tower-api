c6db8411f7c322018cfa5058147b10529986c66a
Tomas Molina <jtml.mass@gmail.com>
2021-02-08 14:38:02 -0400

    First Commit - Basic Configuration

---
+++ README.md
@@ -0,0 +1,1 @@
+"# Edens Tower API" 
---
+++ src/test/java/com/edenstower/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edenstower.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

2c738c6cb0b8a34a666a588a08aa1c51e2cc1394
Robert Dominguez <robertdominguez7@gmail.com>
2021-02-12 14:52:57 -0400

    Add DeleteGame and UpdateGame @GameController

--- src/main/java/com/edenstower/api/controllers/GameController.java
+++ src/main/java/com/edenstower/api/controllers/GameController.java
@@ -10,9 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Date;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 
 @RestController
 @RequestMapping("/games")
@@ -29,22 +27,21 @@ public class GameController {
     public List<Game> getAllGames(){return gameRepository.findAll();}
 
     @GetMapping("/game")
-    public Optional<Game> getGame(@RequestParam String username, @RequestParam String slot){
+    public ResponseEntity getGame(@RequestParam String username, @RequestParam String slot){
         Game.SaveSlot saveSlot = Game.SaveSlot.valueOf(slot);
-        return gameRepository.findById(new GameID(username, saveSlot));
+        if(!gameRepository.existsById(new GameID(username, saveSlot))){
+            return ResponseEntity.notFound().build();
+        }
+        Optional<Game> game = gameRepository.findById(new GameID(username, saveSlot));
+        return  ResponseEntity.ok().body(game);
     }
 
     @PostMapping("/game")
-    public ResponseEntity postGame(@RequestParam String username, @RequestParam String difficulty,
-                                   @RequestParam boolean fullScreen, @RequestParam boolean autoSave,
-                                   @RequestParam int gammaLvl, @RequestParam boolean sfxEnabled,
-                                   @RequestParam int sfxLvl, @RequestParam boolean musicEnabled,
-                                   @RequestParam int musicLvl, @RequestParam int strength,
-                                   @RequestParam int vitality, @RequestParam int defense,
-                                   @RequestParam int speed, @RequestParam int luck,
-                                   @RequestParam long totalKills, @RequestParam long totalDeaths,
-                                   @RequestParam long gameTimeInSeconds, @RequestParam String saveData,
-                                   @RequestParam String saveSlotstr){
+    public ResponseEntity postGame(@RequestParam String username, @RequestParam String difficulty, @RequestParam boolean fullScreen, @RequestParam boolean autoSave,
+                                   @RequestParam int gammaLvl, @RequestParam boolean sfxEnabled, @RequestParam int sfxLvl, @RequestParam boolean musicEnabled,
+                                   @RequestParam int musicLvl, @RequestParam int strength, @RequestParam int vitality, @RequestParam int defense, @RequestParam int speed,
+                                   @RequestParam int luck, @RequestParam long totalKills, @RequestParam long totalDeaths, @RequestParam long gameTimeInSeconds,
+                                   @RequestParam String saveData, @RequestParam String saveSlotstr){
 
         User user = userRepository.findByUsername(username);
         Game.SaveSlot saveSlot = Game.SaveSlot.valueOf(saveSlotstr);
@@ -80,6 +77,65 @@ public class GameController {
         }
         return ResponseEntity.notFound().build();
 
+    }
+
+    @PutMapping("/game")
+    public ResponseEntity updateGame(@RequestParam String username, @RequestParam String difficulty, @RequestParam boolean fullScreen, @RequestParam boolean autoSave,
+                                     @RequestParam int gammaLvl, @RequestParam boolean sfxEnabled, @RequestParam int sfxLvl, @RequestParam boolean musicEnabled,
+                                     @RequestParam int musicLvl, @RequestParam int strength, @RequestParam int vitality, @RequestParam int defense, @RequestParam int speed,
+                                     @RequestParam int luck, @RequestParam long totalKills, @RequestParam long totalDeaths, @RequestParam long gameTimeInSeconds,
+                                     @RequestParam String saveData, @RequestParam String saveSlotstr){
+
+        Game.SaveSlot saveSlot = Game.SaveSlot.valueOf(saveSlotstr);
+        GameID gameID = new GameID(username, saveSlot);
+        Optional<Game> oldGame = gameRepository.findById(gameID);
+
+        if(oldGame.isPresent()){
+            User user = userRepository.findByUsername(username);
+            Game newGame = new Game(
+                    gameID,
+                    user,
+                    saveData,
+                    oldGame.get().getCreatedAt(),
+                    new Date(),
+                    Game.Difficulty.valueOf(difficulty),
+                    gameTimeInSeconds,
+                    fullScreen,
+                    autoSave,
+                    gammaLvl,
+                    sfxEnabled,
+                    sfxLvl,
+                    musicEnabled,
+                    musicLvl,
+                    strength,
+                    vitality,
+                    defense,
+                    speed,
+                    luck,
+                    totalKills,
+                    totalDeaths
+            );
+            gameRepository.save(newGame);
+            return  ResponseEntity.ok().body(newGame);
+        }
+        return ResponseEntity.notFound().build();
+
+    }
+
+    @DeleteMapping("/game")
+    public Map<String, String> deleteGame(@RequestParam String username, @RequestParam String saveSlotstr){
+        Game.SaveSlot saveSlot = Game.SaveSlot.valueOf(saveSlotstr);
+        Map<String, String> response = new HashMap<>();
+        Optional<Game> game = gameRepository.findById(new GameID(username, saveSlot));
+        if(game.isEmpty()){
+            response.put("deleted", "false");
+            response.put("message", "Game not found");
+        }else{
+            gameRepository.delete(game.get());
+            response.put("deleted", "true");
+            response.put("message", "Game has been deleted");
+        }
+        return response;
     }
 
 }

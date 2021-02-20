005809415ecec1af4d4dc9165252bf2b84723800
Tomas Molina <jtml.mass@gmail.com>
2021-02-20 19:06:31 -0400

    Adding GET Games by Username

--- src/main/java/com/edenstower/api/repositories/GameRepository.java
+++ src/main/java/com/edenstower/api/repositories/GameRepository.java
@@ -6,7 +6,10 @@ import com.edenstower.api.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface GameRepository extends JpaRepository<Game, GameID> {
-    
+    public List<Game> findGamesByGameIDPlayerUserName(String username);
+    public boolean existsGameByGameIDPlayerUserName(String username);
 }
